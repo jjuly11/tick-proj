79c7b37ba012fd866720060cdd8336a6f733651f
jjuly <joeljuly11@gmail.com>
2022-12-04 16:02:47 -0400

    started work on siginup

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,10 @@ const User= require("../models/userModel");
 const URLSearchParams= require('url-search-params');
 const Event= require('../models/eventModel');
 
-// const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
+const {santize, sanitize} = require('express-validator/filter');
+// const { findByIdAndUpdate } = 
+const compression = require('compression');
 
 // Login Section
 
@@ -30,10 +33,22 @@ exports.signup= async (req,res,next) => {
         next(err);
     }
 }
-exports.siginUpPost= async (req,res,next) => {
-    try{
+exports.siginUpPost = [
+    check('first_name').isLength({min: 1}).withMessage('First Name Must Be Specified')
+    .isAlphanumeric().withMessage('First Name must be alphanumeric'),
+    
+    check('surname').isLength({ min:1 }).withMessage('Last Name Must be Specified')
+    .isAlphanumeric().withMessage('Last Name Must be Alphanumeric'),
+    
+    check('email').isEmail().withMessage('Invalid Email'),
 
-    }catch(err){
-        next(err);
+    check('password').isLength({min:8}).withMessage('Password Must Be 8 characters long'),
+
+    check('confirm_password').custom((value, {req}) => value === req.body.password ).withMessage('Password Does not match'),
+
+    sanitize('*').trim().escape(),
+
+    (req, res, next) => {
+        
     }
-}
+]
